b07d7c95b31e1a8876bde5c6690d903a9b36b0ed
sethwebster <sethwebster@gmail.com>
2013-01-07 12:35:50 -0700

    Reorganized to include console project

---
+++ GoogleMaps.LocationServices.Console/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMaps.LocationServices.Console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressData[] addresses = new AddressData[] 
+            {
+                new AddressData // Belgium
+                {
+                    Address = "Rue du Cornet 6",
+                    City = "VERVIERS",
+                    State = null,
+                    Country = "Belgium",
+                    Zip = "B-4800"
+                },
+                new AddressData
+                {
+                    Address = "1600 Pennsylvania ave",
+                    City = "Washington",
+                    State = "DC"
+                }
+            };
+
+            var gls = new GoogleLocationService();
+            foreach (var address in addresses)
+            {
+                var latlong = gls.GetLatLongFromAddress(address);
+                var Latitude = latlong.Latitude;
+                var Longitude = latlong.Longitude;
+                System.Console.WriteLine("Address ({0}) is at {1},{2}", address, Latitude, Longitude);
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
